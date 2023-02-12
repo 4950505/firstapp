6ecbe1d6c362348f273b23a219fbea06a5af1803
Arsenii Kolesnichenko <4950505@gmail.com>
2023-02-12 06:54:37 +0300

    umedy task 4 about object

--- script.js
+++ script.js
@@ -1,65 +1,70 @@
 'use strict'
 
-let numberOfFilms;
-
-start();
-
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
-  privat: false,
-}
+  privat: true,
 
-rememberMyFilms();
-detectPersonalLevel();
-writeYourGenres();
-showMyDB(personalMovieDB.privat);
+  start: function() {
+    while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+      personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?');
+    }
+  },
 
-// ========================= funtions
+  rememberMyFilms: function() {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt('Один из последних просмотренных фильмов?', 'Звёздные войны'),
+            b = prompt('На сколько оцените его?', '8.1');
+      if (a && a.length < 50 && b) {
+        personalMovieDB.movies[a] = b;
+        personalMovieDB.count += 1;  
+      } else {
+        alert('Введи нормально сука ебаная');
+        i--;
+      }
+    }
+  },
 
-function start() {
-  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = + prompt('Сколько фильмов вы уже посмотрели?');
-  }
-}
-
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', 'Звёздные войны'),
-          b = prompt('На сколько оцените его?', '8.1');
-    if (a && a.length < 50 && b) {
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.count += 1;  
+  detectPersonalLevel: function() {
+    if (personalMovieDB.count < 10) {
+      console.log('Вы мало лох');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+      console.log('Вы классичский лох');
+    } else if (personalMovieDB.count > 30) {
+      console.log('Да вы кинолох батенька');
     } else {
-      alert('Введи нормально сука ебаная');
-      i--;
+      console.log('товарищ сталин произошла чудовишная БАЖЕНА!');
+    }
+  },
+
+  showMyDB: function() {
+    if(!personalMovieDB.privat) {
+      console.log(personalMovieDB);
+    }
+  },
+ 
+  writeYourGenres: function() {
+    let genres = prompt(`Какие видосики нра? веди через ,`).toLowerCase();
+    while(!genres) {
+      alert('введи норм придурок!');
+      genres = prompt(`Какие видосики нра? веди через ,`);
+    }  
+    personalMovieDB.genres = genres.split(', ');
+    personalMovieDB.genres.sort();
+    personalMovieDB.genres.forEach((element, i) => {
+        console.log(`${i+1}-ый любимйы жанр — ${element} `);
+    });
+    
+  },
+
+  toggleVisibleMyDB: function() {
+    if(personalMovieDB.privat) {
+      personalMovieDB.privat = false;
+    } else {
+      personalMovieDB.privat = true;
     }
   }
-}
 
-function detectPersonalLevel() {
-  if (personalMovieDB.count < 10) {
-    console.log('Вы мало лох');
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    console.log('Вы классичский лох');
-  } else if (personalMovieDB.count > 30) {
-    console.log('Да вы кинолох батенька');
-  } else {
-    console.log('товарищ сталин произошла чудовишная БАЖЕНА!');
-  }
-}
-
-function showMyDB(hidden) {
-  if(!hidden) {
-    console.log(personalMovieDB);
-  }
-}
-
-function writeYourGenres() {
-  for (let i = 0; i < 3; i++) {
-    let genre = prompt(`Какие видосики ${i + 1} нра?`);
-    personalMovieDB.genres[i] = genre;
-  }
 }
